86ba2660e3e823409e3ea3d11571c13da0a7f51a
devnulled <devnulled@cryptaz.org>
2019-04-25 18:00:00 +0700

    EmailService: service for sending email

---
+++ src/service/EmailService.js
@@ -0,0 +1,31 @@
+const nodemailer = require("nodemailer")
+const logger = require("../utils/logger")
+
+class EmailService {
+
+    constructor() {
+        this.transporter = nodemailer.createTransport({
+            secure: false,
+            tls: {rejectUnauthorized: false},
+            host: process.env.SMTP_HOST,
+            port: process.env.SMTP_PORT,
+        })
+
+        this.sendEmail = this.sendEmail.bind(this)
+    }
+
+    async sendEmail(recipent, subject, html) {
+        const info = await this.transporter.sendMail({
+            from: `iVend <root@ivend.pro>`, // sender address
+            to: recipent, // list of receivers
+            subject, // Subject line
+            html
+        })
+
+
+        logger.info(info)
+    }
+
+}
+
+module.exports = EmailService

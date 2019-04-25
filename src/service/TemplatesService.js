cb10fe4a6230a0a44563d010b853a6a97598779c
devnulled <devnulled@cryptaz.org>
2019-04-26 01:52:27 +0700

    TemplatesService: add id to templates

--- src/service/TemplatesService.js
+++ src/service/TemplatesService.js
@@ -6,11 +6,13 @@ class TemplatesService {
     constructor() {
         this.templates = {
             "REGISTRATION_SMS": {
+                id: "REGISTRATION_EMAIL",
                 type: TemplateType.SMS,
                 replacements: ["code"],
                 render: (replacements) => `Проверочный код: ${replacements.code}`
             },
             "REGISTRATION_EMAIL": {
+                id: "REGISTRATION_EMAIL",
                 type: TemplateType.EMAIL,
                 replacements: ["token"],
                 render: (replacements) => `<div><a href="${process.env.FRONTEND_URL}/home?token=${replacements.token}&from=email_confirmed">Пройдите по ссылке чтобы подтвердить учётную запись</a></div>`

a8d6d50e284bb106f3ea0bad320e022c31229da5
ekspil <ekspil@yandex.ru>
2019-12-25 17:38:31 +1000

    Перенес код

--- src/app.js
+++ src/app.js
@@ -8,6 +8,19 @@ const EmailService = require("./service/EmailService")
 const SmsService = require("./service/SmsService")
 const TelegramService = require("./service/TelegramService")
 const TemplatesService = require("./service/TemplatesService")
+const Telegraf = require("telegraf")
+
+const bot = new Telegraf(process.env.TELEGRAM_TOKEN)
+const knex = require("knex")({
+    client: "pg",
+    connection: {
+        host: process.env.POSTGRES_HOST,
+        user: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DB,
+        ssl: true
+    }
+})
 
 const Routes = require("./routes")
 const logger = require("my-custom-logger")
@@ -18,7 +31,7 @@ const fastify = require("fastify")({
 const smsService = new SmsService({})
 const emailService = new EmailService({})
 const templatesService = new TemplatesService({})
-const telegramService = new TelegramService({})
+const telegramService = new TelegramService({knex, bot})
 
 Routes({fastify, emailService, smsService, templatesService, telegramService})
 
